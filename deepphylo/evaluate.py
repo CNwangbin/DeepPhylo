7026bb49283d26eb4b9010e28f8106003666a86f
1143long <1551203328@qq.com>
2024-02-21 18:56:38 +0800

    add evaluate.py

---
+++ deepphylo/evaluate.py
@@ -0,0 +1,44 @@
+import numpy as np
+from sklearn.metrics import  accuracy_score, matthews_corrcoef, precision_recall_curve, auc, roc_curve
+
+
+def select_best_epoch(metrics_dict, metrics_order):
+    filtered_epochs = None
+    for metric in metrics_order:
+        if not metrics_dict[metric]:
+            continue
+        if len(metrics_dict[metric]) == 1:
+            filtered_epochs = metrics_dict[metric][0]
+            break
+        if filtered_epochs is not None:
+            filtered_metric = [(epoch, val) for epoch, val in enumerate(metrics_dict[metric]) if epoch in filtered_epochs]
+            max_val = max(filtered_metric, key=lambda x: x[1])
+            filtered_epochs = [epoch for epoch, val in filtered_metric if val == max_val[1]]
+            if len(filtered_epochs) == 1:
+                best_epoch = filtered_epochs[0]
+                break
+        else:
+            max_val = max(enumerate(metrics_dict[metric]), key=lambda x: x[1])
+            filtered_epochs = [epoch for epoch, val in enumerate(metrics_dict[metric]) if val == max_val[1]]
+            if len(filtered_epochs) == 1:
+                best_epoch = filtered_epochs[0]
+                break
+    return best_epoch
+
+def compute_metrics(y_true, y_pred):
+    if not isinstance(y_true, np.ndarray):
+        y_true = np.array(y_true)
+    if not isinstance(y_pred, np.ndarray):
+        y_pred = np.array(y_pred)
+    y_pred_hard = np.where(y_pred > 0.5, 1, 0)
+    acc = accuracy_score(y_true, y_pred_hard)
+    mcc = matthews_corrcoef(y_true, y_pred_hard)
+    fpr, tpr, t = roc_curve(y_true, y_pred)
+    roc_auc = auc(fpr, tpr)
+    # 从FPR和TPR计算特异度和敏感度
+    specificity = 1 - fpr
+    sensitivity = tpr
+    precision, recall, thresholds = precision_recall_curve(y_true, y_pred)
+    aupr = auc(recall, precision)
+    metric_dict = {'acc': acc, 'mcc': mcc, 'roc_auc': roc_auc, 'aupr': aupr, 'precision':precision,'recall':recall, 'specificity':specificity, 'sensitivity':sensitivity}
+    return metric_dict
